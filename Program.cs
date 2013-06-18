b3ba2f8b92fcee339c851be43187fd5b5b1ec919
Alvaro Rojas <alvaro.rojas@tierconnect.com>
2013-06-18 14:43:09 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using Tesseract;
+
+namespace TesseractOCR.Tests.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testImagePath = "./phototest.tif";
+            var img = new Bitmap(testImagePath);
+            
+            System.Console.WriteLine(img.Width);
+            System.Console.WriteLine(img.Height);
+            
+            TesseractOCRFax tess = new TesseractOCRFax(img);
+            Rectangle rect = new Rectangle(0, 0, 800, 800);
+            
+            //System.Console.WriteLine(tess.getText());
+            System.Console.WriteLine(tess.getTextByRectangle(rect));
+
+
+            //using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+            //{
+            //    using (var img = Pix.LoadFromFile("./faxsample.tif"))
+            //    {
+            //        using (var page = engine.Process(img))
+            //        {
+            //            var text = page.GetText();
+            //            System.Console.WriteLine(text);
+            //            using (var iter = page.GetIterator())
+            //            {
+            //                iter.Begin();
+            //                do
+            //                {
+
+            //                } while (iter.Next(PageIteratorLevel.Para, PageIteratorLevel.TextLine));
+            //            }
+            //        }
+            //    }
+            //}
+            System.Console.ReadKey();
+        }
+    }
+
+}
---
+++ TesseractOCRFax.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Tesseract;
+
+namespace TesseractOCR.Tests.Console
+{
+    class TesseractOCRFax
+    {
+        private Tesseract.TesseractEngine engine = null;
+        private Bitmap faxImage = null;
+
+
+        public TesseractOCRFax(Bitmap image)
+        {
+            faxImage = image;
+            engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default);
+        }
+
+        public string getText(Bitmap image)
+        {
+            string res = "";
+            using (var page = engine.Process(image))
+            {
+                res = page.GetText();
+            }
+            return res;
+        }
+
+        //Return the fax text of the entire fax
+        public string getText()
+        {
+            return getText(faxImage);
+        }
+
+        public string getTextByRectangle(Rectangle rectangle)
+        {
+            Bitmap bmpCrop = faxImage.Clone(rectangle, faxImage.PixelFormat);
+            return getText(bmpCrop);
+        }
+    }
+}
